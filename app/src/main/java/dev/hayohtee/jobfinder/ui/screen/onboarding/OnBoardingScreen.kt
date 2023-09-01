7fd8ddd5724d0c9267b2c8af1c31c661ef35c1c1
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-09-01 16:41:29 +0100

    Move OnBoardingScreen.kt into its own package

--- app/src/main/java/dev/hayohtee/jobfinder/ui/screen/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/dev/hayohtee/jobfinder/ui/screen/onboarding/OnBoardingScreen.kt
@@ -1,4 +1,4 @@
-package dev.hayohtee.jobfinder.ui.screen
+package dev.hayohtee.jobfinder.ui.screen.onboarding
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
