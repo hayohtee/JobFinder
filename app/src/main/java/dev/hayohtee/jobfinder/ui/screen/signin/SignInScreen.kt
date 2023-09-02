9940afa5a88744978d5432cef484d4a7a27e3a9e
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-09-02 13:44:55 +0100

    Update SignInScreen

--- app/src/main/java/dev/hayohtee/jobfinder/ui/screen/signin/SignInScreen.kt
+++ app/src/main/java/dev/hayohtee/jobfinder/ui/screen/signin/SignInScreen.kt
@@ -7,13 +7,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -21,7 +17,6 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -31,41 +26,41 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import dev.hayohtee.jobfinder.R
+import dev.hayohtee.jobfinder.ui.component.SocialButtonsGroup
 import dev.hayohtee.jobfinder.ui.theme.JobFinderTheme
 
 @Composable
 fun SignInScreen(
     emailAddress: String,
-    onEmailAddressChanged: (String) -> Unit,
+    onEmailAddressChange: (String) -> Unit,
     password: String,
-    onPasswordChanged: (String) -> Unit,
-    onSignInClicked: () -> Unit,
-    onForgetPasswordClicked: () -> Unit,
-    onGoogleClicked: () -> Unit,
-    onFaceBookClicked: () -> Unit,
-    onCreateAccountClicked: () -> Unit,
+    onPasswordChange: (String) -> Unit,
+    onSignInClick: () -> Unit,
+    onForgetPasswordClick: () -> Unit,
+    onGoogleClick: () -> Unit,
+    onFaceBookClick: () -> Unit,
+    onCreateAccountClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column(
         modifier = modifier
             .safeDrawingPadding()
             .padding(dimensionResource(id = R.dimen.medium)),
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.medium))
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.large))
     ) {
         SignInHeader(modifier = Modifier.fillMaxWidth(0.7f))
         SignInFields(
             emailAddress = emailAddress,
-            onEmailAddressChanged = onEmailAddressChanged,
+            onEmailAddressChange = onEmailAddressChange,
             password = password,
-            onPasswordChanged = onPasswordChanged,
-            onForgetPasswordClicked = onForgetPasswordClicked,
-            onSignInClicked = onSignInClicked
+            onPasswordChange = onPasswordChange,
+            onForgetPasswordClick = onForgetPasswordClick,
+            onSignInClick = onSignInClick
         )
         SocialButtonsGroup(
-            onGoogleClicked = onGoogleClicked,
-            onFaceBookClicked = onFaceBookClicked
+            onGoogleClick = onGoogleClick,
+            onFaceBookClick = onFaceBookClick
         )
-
         Row(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier.align(Alignment.CenterHorizontally)
@@ -74,7 +69,7 @@ fun SignInScreen(
                 text = stringResource(id = R.string.is_new_user),
                 style = MaterialTheme.typography.bodyLarge
             )
-            TextButton(onClick = onCreateAccountClicked) {
+            TextButton(onClick = onCreateAccountClick) {
                 Text(
                     text = stringResource(id = R.string.create_account),
                     style = MaterialTheme.typography.bodyLarge
@@ -108,20 +103,20 @@ fun SignInHeader(modifier: Modifier = Modifier) {
 @Composable
 fun SignInFields(
     emailAddress: String,
-    onEmailAddressChanged: (String) -> Unit,
+    onEmailAddressChange: (String) -> Unit,
     password: String,
-    onPasswordChanged: (String) -> Unit,
-    onForgetPasswordClicked: () -> Unit,
-    onSignInClicked: () -> Unit,
+    onPasswordChange: (String) -> Unit,
+    onForgetPasswordClick: () -> Unit,
+    onSignInClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column(
         modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.medium))
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.large))
     ) {
         OutlinedTextField(
             value = emailAddress,
-            onValueChange = onEmailAddressChanged,
+            onValueChange = onEmailAddressChange,
             singleLine = true,
             keyboardOptions = KeyboardOptions.Default.copy(
                 keyboardType = KeyboardType.Email,
@@ -139,88 +134,40 @@ fun SignInFields(
                 )
             }
         )
-        OutlinedTextField(
-            value = password,
-            onValueChange = onPasswordChanged,
-            singleLine = true,
-            keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType = KeyboardType.Password,
-                imeAction = ImeAction.Done
-            ),
-            placeholder = {
-                Text(text = stringResource(id = R.string.password))
-            },
-            modifier = Modifier.fillMaxWidth(),
-            leadingIcon = {
-                Icon(
-                    painter = painterResource(id = R.drawable.lock_keyhole_svgrepo),
-                    contentDescription = null,
-                    modifier = Modifier.size(dimensionResource(id = R.dimen.textfield_icon_size))
-                )
+        Column {
+            OutlinedTextField(
+                value = password,
+                onValueChange = onPasswordChange,
+                singleLine = true,
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Done
+                ),
+                placeholder = {
+                    Text(text = stringResource(id = R.string.password))
+                },
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.lock_keyhole_svgrepo),
+                        contentDescription = null,
+                        modifier = Modifier.size(dimensionResource(id = R.dimen.textfield_icon_size))
+                    )
+                }
+            )
+            TextButton(
+                onClick = onForgetPasswordClick,
+                modifier = Modifier.align(Alignment.End)
+            ) {
+                Text(text = stringResource(id = R.string.forget_password))
             }
-        )
-        TextButton(onClick = onForgetPasswordClicked, modifier = Modifier.align(Alignment.End)) {
-            Text(text = stringResource(id = R.string.forget_password))
         }
-        Button(onClick = onSignInClicked, modifier = Modifier.fillMaxWidth()) {
+        Button(onClick = onSignInClick, modifier = Modifier.fillMaxWidth()) {
             Text(text = stringResource(id = R.string.sign_in))
         }
     }
 }
 
-@Composable
-fun SocialButtonsGroup(
-    onGoogleClicked: () -> Unit,
-    onFaceBookClicked: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Column(
-        modifier = modifier.fillMaxWidth(),
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.medium)),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.small))
-        ) {
-            Divider(modifier = Modifier.width(dimensionResource(id = R.dimen.divider_width)))
-            Text(
-                text = stringResource(id = R.string.continue_with_label),
-                style = MaterialTheme.typography.bodyLarge
-            )
-            Divider(modifier = Modifier.width(dimensionResource(id = R.dimen.divider_width)))
-        }
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.medium))
-        ) {
-            IconButton(
-                onClick = onGoogleClicked,
-                colors = IconButtonDefaults.iconButtonColors(
-                    contentColor = Color.Unspecified
-                )
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.google_svgrepo),
-                    contentDescription = stringResource(id = R.string.google),
-                    modifier = Modifier.size(dimensionResource(id = R.dimen.icon_button_size))
-                )
-            }
-            IconButton(
-                onClick = onFaceBookClicked,
-                colors = IconButtonDefaults.iconButtonColors(
-                    contentColor = Color.Unspecified,
-                )
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.facebook_svgrepo),
-                    contentDescription = stringResource(id = R.string.facebook),
-                    modifier = Modifier.size(dimensionResource(id = R.dimen.icon_button_size))
-                )
-            }
-        }
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
@@ -228,14 +175,14 @@ fun SignupScreenPreview() {
     JobFinderTheme {
         SignInScreen(
             emailAddress = "",
-            onEmailAddressChanged = {},
+            onEmailAddressChange = {},
             password = "",
-            onPasswordChanged = {},
-            onSignInClicked = {},
-            onForgetPasswordClicked = {},
-            onGoogleClicked = {},
-            onFaceBookClicked = {},
-            onCreateAccountClicked = {},
+            onPasswordChange = {},
+            onSignInClick = {},
+            onForgetPasswordClick = {},
+            onGoogleClick = {},
+            onFaceBookClick = {},
+            onCreateAccountClick = {},
             modifier = Modifier.padding(dimensionResource(id = R.dimen.medium))
         )
     }
